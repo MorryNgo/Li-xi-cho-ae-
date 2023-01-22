c3b456dfd4c6051a8413168831c851ed21389dff
MorryNgo <103199602+MorryNgo@users.noreply.github.com>
2023-01-22 13:16:45 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -3,27 +3,29 @@ const thiep= document.querySelector("body > div > div.bia")
 const nap = document.querySelector("body > div > div.nap");
 const lixi = document.querySelector("body > div > div.li-xi")
 const song = document.getElementById("song")
-nap.addEventListener("click",open)
 
+nap.addEventListener("click",open)
 function open(){
     nap.style.animation='open linear 2s forwards';
-    setTimeout(function(){lixi.style.animation='push-up linear 2s forwards';   nap.style.zIndex=0},3000)
+    setTimeout(function(){lixi.style.animation='push-up linear 2s forwards';   
+    nap.style.zIndex=0;},3000)
     setTimeout(function(){
         thiep.style.animation='disappeared linear 2s forwards'
         box.style.background= 'transparent'
         nap.style.animation='disappeared linear 2s forwards'
+        setTimeout(() => {
+            nap.remove()
+            thiep.remove()
+            song.load()
+            song.play();    
+            lixi.style.background="url(https://github.com/MorryNgo/li-xi/raw/main/unknown_1.png)"
+            lixi.style.backgroundSize="cover"
+            box.style.height = "0"
+            box.style.width="auto"
+            box.style.top = "400px"
+            lixi.style.height="500px"
+            lixi.style.width="600px"
+           }, 3000);
     },5000)
-    setTimeout(function(){
-        lixi.innerHTML='a'
-        lixi.style.animation='sizeup linear 3s forwards'
-            setTimeout(() => {
-                lixi.innerHTML='a iu'
-            }, 2000);
-            setTimeout(() => {
-                lixi.innerHTML='a iu e'
-            }, 3000);
-            song.play()
-            song.loop = true;
-},7500)
 }
 
